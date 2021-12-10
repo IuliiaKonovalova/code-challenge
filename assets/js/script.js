0b741422cd7cf3ebae0e6bd5d89539a5dd2a0987
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-10 12:26:54 +0800

    Import libraries

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* Import Libraries */
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const execSync = require('child_process').execSync;
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
